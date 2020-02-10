de5063c320ec358f6c5482011cbb50e31f01e178
Harish Sune <harishsune@Hotmail.com>
2020-02-10 15:12:20 +0530

    Added changes

---
+++ build/precache-manifest.63e782e54eb662b3b7f24d2c1604b402.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d8848cff28249bb8932ad63349ef2b",
+    "url": "/stunning-lamp/index.html"
+  },
+  {
+    "revision": "e39560ef9769511ebc2d",
+    "url": "/stunning-lamp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8efac415d1c905f84949",
+    "url": "/stunning-lamp/static/js/2.c1a74582.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/stunning-lamp/static/js/2.c1a74582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39560ef9769511ebc2d",
+    "url": "/stunning-lamp/static/js/main.bd0fc990.chunk.js"
+  },
+  {
+    "revision": "898f65efb98cc513ae44",
+    "url": "/stunning-lamp/static/js/runtime-main.9730d8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stunning-lamp/static/media/logo.5d5d9eef.svg"
+  }
+]);
